41852d341fbdcd0636061e1df07662ac8bf751a2
Zachary <zdean1995@gmail.com>
2023-04-22 17:22:05 -0400

    Updated readme

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# JAV1001-Final-Project-Fragment
+# Pizza Time - Final Project for JAV1001
+Zachary Dean
+A00275392
+
+An app for ordering pizzas.  Not finished, unfortunatly.
+
--- app/src/main/java/com/auchtermuchty/pizzatime_finalproject/adapter/ToppingsAdapter.kt
+++ app/src/main/java/com/auchtermuchty/pizzatime_finalproject/adapter/ToppingsAdapter.kt
@@ -9,7 +9,9 @@ import com.auchtermuchty.pizzatime_finalproject.CreateOrderFragment
 import com.auchtermuchty.pizzatime_finalproject.R
 import com.auchtermuchty.pizzatime_finalproject.model.PizzaTimeViewModel
 
-//adapter for the toppings recycler view in the create order fragment
+//adapter for the toppings recycler view in the create order fragment.  Passing in the fragment itself
+//fixed the issue with adding or removing toppings not updating the ui.  This is probably really stupid
+//and really limits the use of the adapter to just that fragment.
 class ToppingsAdapter(var frag: CreateOrderFragment, var viewModel: PizzaTimeViewModel):
     RecyclerView.Adapter<ToppingsAdapter.ToppingsViewHolder>() {
     //the list of all toppings
